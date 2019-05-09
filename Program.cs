ae504cfebcafe15e9733d0885d11eda71ce3633d
stonepresto <11134586+stonepresto@users.noreply.github.com>
2019-05-08 18:13:25 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CLMB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            System.Management.Automation.Runspaces.Runspace run = System.Management.Automation.Runspaces.RunspaceFactory.CreateRunspace();
+            run.Open();
+
+            System.Management.Automation.PowerShell shell = System.Management.Automation.PowerShell.Create();
+            shell.Runspace = run;
+
+            String exec = args[0];
+            shell.AddScript(exec);
+            shell.Invoke();
+
+            run.Close();
+        }
+    }
+}
